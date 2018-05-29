7ea7e0e51f944dda6575beed1bf35f36120bd27c
licontre <carralero@openmailbox.org>
2018-05-29 15:59:04 +0200

    -Soluciionado Secuencia

--- GrupoA-ejb/src/java/modeloJPA/Usuario.java
+++ GrupoA-ejb/src/java/modeloJPA/Usuario.java
@@ -25,7 +25,7 @@ public class Usuario implements Serializable {
 
     private static long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;   
     public enum Sexo { HOMBRE , MUJER };
     private String nombreusuario;
